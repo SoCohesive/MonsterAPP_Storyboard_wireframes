bf948f44c6189323b853a464b5f7cc624cb6c77f
Sonam Dhingra <sdhingra89@gmail.com>
2013-06-08 10:55:20 -0500

    first round of wireframes with all steps

---
+++ MonsterAPP_Storyboard_Wireframes/MonsterAPP_Storyboard_Wireframes/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MonsterAPP_Storyboard_Wireframes
+//
+//  Created by Sonam Dhingra on 6/8/13.
+//  Copyright (c) 2013 Sonam Dhingra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ MonsterAPP_Storyboard_Wireframes/MonsterAPP_Storyboard_Wireframes/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  MonsterAPP_Storyboard_Wireframes
+//
+//  Created by Sonam Dhingra on 6/8/13.
+//  Copyright (c) 2013 Sonam Dhingra. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
